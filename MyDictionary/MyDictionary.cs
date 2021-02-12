232238008375d9fe26a959da0bb48820ddeb0662
Merve Karabekmez <mervekarabekmezz@gmail.com>
2021-02-12 21:22:39 +0300

    create

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Dictionary<TKey,TValue> //dictionary hem anahtar,hem değerleri tutar
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public Dictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key ,TValue value)
+        {
+            TKey[] tempArray1 = keys;
+            TValue[] temArray2 = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                keys[i] = tempArray1[i];
+                values[i] = temArray2[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public void List()
+        {
+            for (int i = 0; i < keys. Length; i++)
+            {
+                Console.WriteLine(keys[i]+ " " + values[i]);
+            }
+        }
+    }
+}
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> myDictionary = new Dictionary<int, string>();
+            myDictionary.Add(1,"Merve Karabekmez");
+            myDictionary.Add(2, "Mustafa Karabekmez");
+            myDictionary.Add(5, "Kemal Sunal");
+            myDictionary.Add(6, "Halit Akçatepe");
+            myDictionary.Add(12, "Zeki Alasya");
+            myDictionary.Add(4, "Tarık Akan");
+
+            myDictionary.List();
+        }
+
+        
+    }
+
+   
+}
